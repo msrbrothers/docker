79a2557691c925c5a87f1f6591b900e6b633634c
msrbrothers <ravindralodhi987@gmail.com>
2025-01-11 20:09:07 +0530

    docker compose

--- micro-service1/server.js
+++ micro-service1/server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.get('/',(req,res)=>{
   res.status(200).json({
-    message : "node server running"
+    message : "Hello from M1"
   })
 })
 
---
+++ micro-service3/server.js
@@ -0,0 +1,14 @@
+import express from 'express'
+
+const app = express();
+
+
+app.get('/',(req,res)=>{
+  res.status(200).json({
+    message : "Hello  M3"
+  })
+})
+
+app.listen(3000, ()=>{
+    console.log('Server running on 3000');
+})
